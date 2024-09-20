8fd35d2bbc63b1af73d029775a37a422553b318b
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-09-19 22:27:30 -0600

    Inicio de la app web prototipo // desarrollo del fronted

---
+++ ERP/Pages/Empleado/Empleado_list.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERP.Pages.Empleado
+{
+    public class Empleado_listModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
