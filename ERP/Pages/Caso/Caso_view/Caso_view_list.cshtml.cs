ce0623712562bef511738351e81cf3d7cba2a2b6
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-09-23 12:07:28 -0600

    Fronted: Finalización del formato y formulario de Casos.

---
+++ ERP/Pages/Caso/Caso_view/Caso_view_list.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERP.Pages.Caso.Caso_view
+{
+    public class Caso_view_listModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ERP/Pages/Caso/Reporte/Menu_Reporte.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERP.Pages.Caso.Reporte
+{
+    public class Menu_ReporteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
