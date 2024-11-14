f1c4fb1d832703414de03b4047ca7519a8d73231
jimendezm <132164589+jimendezm@users.noreply.github.com>
2024-11-13 23:02:28 -0600

    update

---
+++ ERP/Pages/Planilla/ReportesPlanilla/ReporteMesAño.cshtml.cs
@@ -0,0 +1,55 @@
+// Modelo: ReporteMesAñoModel.cs
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+
+namespace ERP.Pages.Planilla.ReportesPlanilla
+{
+    public class ReporteMesAñoModel : PageModel
+    {
+        public List<PlanillaMesAñoData> PlanillaMesAñoData { get; set; } = new List<PlanillaMesAñoData>();
+        public string mensajeError = "";
+
+        public void OnPost()
+        {
+            string fechaInicio = Request.Form["FechaInicio"];
+            string fechaFin = Request.Form["FechaFin"];
+
+            try
+            {
+                Conexion conexionBD = new Conexion();
+                conexionBD.abrir();
+                string query = "SELECT AñoMes, MontoTotal FROM ObtenerMontosPlanillaRangoFechas(@FechaInicio, @FechaFin)";
+                SqlCommand command = conexionBD.obtenerComando(query);
+                command.Parameters.AddWithValue("@FechaInicio", fechaInicio);
+                command.Parameters.AddWithValue("@FechaFin", fechaFin);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        PlanillaMesAñoData.Add(new PlanillaMesAñoData
+                        {
+                            AñoMes = reader.GetString(0),
+                            MontoTotal = reader.GetDouble(1).ToString()
+                        });
+                    }
+                }
+                conexionBD.cerrar();
+            }
+            catch (Exception ex)
+            {
+                mensajeError = "Error al obtener los datos de la base de datos: " + ex.Message;
+            }
+        }
+    }
+
+    public class PlanillaMesAñoData
+    {
+        public string AñoMes { get; set; }
+        public string MontoTotal { get; set; }
+    }
+}
